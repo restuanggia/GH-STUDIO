7b6877777a98d9ba3866e08ab8ce6ec46beb725b
restuanggia <restuanggia56@gmail.com>
2025-01-11 15:13:35 +0700

    restuanggia: menambahkan gambar pada about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export default function page() {
+export default function AboutPage() {
   return (
-    <div>page</div>
-  )
+    <>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="https://palpres.disway.id/upload/bef2288175b986703f8cbf59831588ee.jpeg"
+            className="max-w-sm rounded-lg shadow-2xl" />
+        </div>
+      </div>
+    </>
+  );
 }
