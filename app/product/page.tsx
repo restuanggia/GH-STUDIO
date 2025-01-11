0949a1fc883d5e081d91ab9fed62d682a009441f
restuanggia <restuanggia56@gmail.com>
2025-01-11 15:58:43 +0700

    restuanggia: Menambahkan Data Product untuk di Tampilkan pada file product/page.tsx

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -2,18 +2,27 @@
 import { useState } from "react";
 
 export default function ProductPage() {
-  const [searchQuery, setSearchQuery] = useState("");
+    const [searchQuery, setSearchQuery] = useState("");
 
-  const products = [
-    {
-      id: 1,
-      title: "Laptop",
-      description:
-        "Kamu lagi nyari-nyari laptop yang cocok buat analisis data? Yuk, cek rekomendasi laptop-laptop terbaik untuk data analyst berikut ...",
-      image:
-        "https://dibimbing-cdn.sgp1.cdn.digitaloceanspaces.com/1693586160053-10-Rekomendasi-Laptop-Terbaik-untuk-Data-Analyst-2023.webp",
-      link: "https://dibimbing.id/blog/detail/10-rekomendasi-laptop-terbaik-untuk-data-analyst-2023",
-      },
+    const products = [
+        {
+            id: 1,
+            title: "Laptop",
+            description:
+                "Kamu lagi nyari-nyari laptop yang cocok buat analisis data? Yuk, cek rekomendasi laptop-laptop terbaik untuk data analyst berikut ...",
+            image:
+                "https://dibimbing-cdn.sgp1.cdn.digitaloceanspaces.com/1693586160053-10-Rekomendasi-Laptop-Terbaik-untuk-Data-Analyst-2023.webp",
+            link: "https://dibimbing.id/blog/detail/10-rekomendasi-laptop-terbaik-untuk-data-analyst-2023",
+        },
+        {
+            id: 2,
+            title: "Monitor",
+            description:
+                "LG Monitor meningkatkan akurasi warna melalui layar LCD. Layar 21,45 inci dengan Full HD (1920x1080) dapat meningkatkan ...",
+            image:
+                "https://www.lg.com/content/dam/channel/wcms/id/images/monitor/22mr410-b_atiq_eain_id_c/gallery/large03.jpg",
+            link: "https://www.lg.com/id/monitor/fhd-qhd/22mr410-b/",
+        },
     ];
 
 }
