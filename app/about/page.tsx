0a75f68d51e2d4a3f78abfe3b4a3d0c39b7d4b7d
Rama Wijaya <ramawijaya518yh@gmai.com>
2025-01-11 15:19:59 +0700

    Rama : Menambahkan Text Pada About

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function AboutPage() {
   return (
     <>
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
-          <img
-            src="https://palpres.disway.id/upload/bef2288175b986703f8cbf59831588ee.jpeg"
-            className="max-w-sm rounded-lg shadow-2xl" />
+          <img src="https://palpres.disway.id/upload/bef2288175b986703f8cbf59831588ee.jpeg" className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Tentang TechMart!</h1>
+            <p className="py-6">
+              TechMart hadir sebagai solusi terbaik untuk memenuhi berbagai kebutuhan elektronik Anda, mulai dari perangkat canggih hingga aksesoris modern yang mendukung gaya hidup digital masa kini. Kami memahami bahwa teknologi bukan
+              hanya tentang perangkat, tetapi juga tentang memberikan kemudahan, efisiensi, dan kenyamanan dalam setiap aspek kehidupan Anda. Produk Berkualitas untuk Semua Kalangan TechMart menyediakan beragam produk berkualitas tinggi
+              yang dirancang untuk berbagai kebutuhan. Mulai dari laptop dan komputer canggih untuk para profesional, pelajar, hingga gamer, hingga smartphone dan gadget terkini yang memudahkan komunikasi dan aktivitas sehari-hari Anda.
+              Tidak hanya itu, kami juga menawarkan monitor beresolusi tinggi, gaming gear premium, hingga peralatan rumah tangga elektronik yang modern dan efisien.
+            </p>
+          </div>
         </div>
       </div>
     </>
