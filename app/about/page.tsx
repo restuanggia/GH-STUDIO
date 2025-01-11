0f4bac4620979fa4841425ed6ad083edf54a45da
restuanggia <restuanggia56@gmail.com>
2025-01-11 15:36:18 +0700

    restuanggia: menambahkan teks pada about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,9 @@ export default function AboutPage() {
     <>
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
-          <img src="https://palpres.disway.id/upload/bef2288175b986703f8cbf59831588ee.jpeg" className="max-w-sm rounded-lg shadow-2xl" />
+          <img
+            src="https://palpres.disway.id/upload/bef2288175b986703f8cbf59831588ee.jpeg"
+            className="max-w-sm rounded-lg shadow-2xl" />
           <div>
             <h1 className="text-5xl font-bold">Tentang TechMart!</h1>
             <p className="py-6">
@@ -22,6 +24,14 @@ export default function AboutPage() {
           <img
             src="https://ceklist.id/wp-content/uploads/2023/05/Toko-Komputer-dan-Laptop-Terpercaya.jpg"
             className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Solusi Teknologi Berkualitas dan Terpercaya</h1>
+            <p className="py-6">
+              TechMart menyediakan beragam produk berkualitas tinggi yang dirancang untuk memenuhi kebutuhan berbagai kalangan, mulai dari laptop dan komputer canggih untuk profesional, pelajar, hingga gamer, serta smartphone dan gadget terkini yang memudahkan komunikasi dan aktivitas sehari-hari. Selain itu, kami juga menawarkan monitor beresolusi tinggi, gaming gear premium,
+              hingga peralatan rumah tangga elektronik yang modern dan efisien. Sebagai pilihan terpercaya untuk teknologi masa depan, TechMart hanya menyediakan produk dari merek-merek ternama yang telah teruji kualitasnya. Setiap produk kami telah melalui proses kurasi yang ketat untuk memastikan Anda mendapatkan perangkat yang tahan lama, fungsional, dan sesuai dengan kebutuhan,
+              sehingga teknologi dapat membantu Anda mencapai potensi terbaik.
+            </p>
+          </div>
         </div>
       </div>
     </>
