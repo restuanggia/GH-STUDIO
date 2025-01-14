902d8f03b7dac242ececeaa280ac27d1280aa8e0
restuanggia <restuanggia56@gmail.com>
2025-01-14 21:21:37 +0700

    restuanggia: menambahkan data product

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -27,10 +27,19 @@ export default function ProductPage() {
             id: 3,
             title: "Mouse",
             description:
-              "Pasti dari kalian semua sering banget mendengar kata Onboard Memory dalam mouse gaming !!! Tapi tahukah kalian sebenarnya tentang pengertian onboard ...",
+                "Pasti dari kalian semua sering banget mendengar kata Onboard Memory dalam mouse gaming !!! Tapi tahukah kalian sebenarnya tentang pengertian onboard ...",
             image: "https://www.nyk.co.id/wp-content/uploads/2021/04/s4.jpeg",
             link: "https://www.nyk.co.id/onboard-memory-pada-mouse-gaming/",
-          },
+        },
+        {
+            id: 4,
+            title: "Keyboard",
+            description:
+                "Keyboard merupakan perangkat yang paling penting keberadaannya setelah mouse. Saking pentingnya bisa dibilang, kedua perangkat tersebut tidak boleh dihilangkan ...",
+            image:
+                "https://www.filemagz.com/wp-content/uploads/2021/10/keyboard-header-min-scaled.jpg",
+            link: "https://www.filemagz.com/jenis-keyboard-komputer-dari-bentuknya/",
+        },
 
     ];
 
